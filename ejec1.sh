d28b51547f27ea37ae335d0f88da6c3a48a87228
adfmb <actuario.alfredo.mb@gmail.com>
2017-05-23 03:46:19 -0500

    archivos docker 03

--- ejec1.sh
+++ ejec1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ./vamb.sh
+. ./ambiente/vamb.sh
 
 docker-machine create awsn00                            
 docker-machine ssh awsn00 sudo usermod -aG docker ubuntu
@@ -11,6 +11,7 @@ aws ec2 authorize-security-group-ingress --group-name docker-machine --protocol 
 docker info | grep ^Name
 
 docker-machine ssh awsn00 git clone https://github.com/adfmb/vis4googleT.git
+docker-machine scp -r ambiente/vamb.sh awsn00:/home/ubuntu/vis4googleT/ambiente/vamb.sh
 docker-machine ssh awsn00 chmod u+x vis4googleT/execn1_01.sh
 docker-machine ssh awsn00 ./vis4googleT/execn1_01.sh #Este script termina con el swarm inicializado
 
