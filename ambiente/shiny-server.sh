83a09cf104d1a404cafc5e1de73cd9f307c95e51
adfmb <actuario.alfredo.mb@gmail.com>
2017-05-23 23:24:15 -0500

    cambios para ejecutar vamb.sh 01

--- ambiente/shiny-server.sh
+++ ambiente/shiny-server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exportando variables de ambiente de llaves de aws
+. ./tmp/vamb.sh
+
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
